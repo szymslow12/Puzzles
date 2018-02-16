03c05f1949566210e5521ad07e260be79690cef9
szymslow12 <szym.slow12@gmail.com>
2018-02-16 13:25:37 +0100

    Day6 part1 solved

---
+++ Day6/day6_part1.py
@@ -0,0 +1,27 @@
+def redistibute_banks(filename):
+    banks = []
+    with open(filename, 'r') as f:
+        for bank in f.readlines():
+            banks.append(bank.strip().split('\t'))
+    for bank in banks:
+        for i in range(len(bank)):
+            bank[i] = int(bank[i])
+    states = [banks[0]]
+    cycle = 0
+    while True:
+        new_state = [block for block in states[-1]]
+        cycle += 1
+        position = new_state.index(max(new_state))
+        blocks = new_state[position]
+        new_state[position] = 0
+        while blocks > 0:
+            position = (position + 1) % len(new_state)
+            new_state[position] += 1
+            blocks -= 1
+        if new_state in states:
+            return cycle
+        else:
+            states.append(new_state)
+
+
+print(redistibute_banks('day6_input.txt'))
